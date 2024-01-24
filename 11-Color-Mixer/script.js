eeccd5efda947766a3b2d97ddcbf4844b77ebc8f
Katharina-Beck <katja131291@gmail.com>
2024-01-24 17:05:59 +0100

    add: struktur for the following tasks

---
+++ 11-Color-Mixer/script.js
@@ -0,0 +1,14 @@
+const output = document.querySelector(".output");
+const ranges = document.querySelectorAll("input[type=range]");
+const colorCode = document.querySelector(".colorCode");
+updateColor();
+ranges.forEach((range) => range.addEventListener("input", updateColor));
+
+function updateColor() {
+  const color = `rgb(${ranges[0].value}, ${ranges[1].value}, ${ranges[2].value}) `;
+  output.style.backgroundColor = color;
+  colorCode.innerText = color;
+}
+
+
+
